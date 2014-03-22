c653d34d423471f192281ad29c8b9d0256751055
evasnow <evasnow@gmail.com>
2014-03-21 19:02:05 -0600

    deleting require.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-define(['views/die-view'], function() {
-	
-	var init = function() {
-		var diceArray = [
-			{roll: ''},
-			{roll: ''}
-		];
-		appDice = new app.views.Dice(diceArray);
-		//appRouter = new app.routers.Router();
-		//Backbone.history.start();
-	};
-	
-	return {
-		init: init
-	};
-});
+app.init();
--- js/views/die-view.js
+++ js/views/die-view.js
@@ -1,44 +1,39 @@
-define(['text!templates/die-template.html', 'models/dice-model'], function() {
+app = app || {};
 
-	var dieView = Backbone.View.extend({
+app.views.Die = Backbone.View.extend({
 
-		el : $("#content"),
+	el : $("#content"),
 
-		render : function() {
-			var that = this;
-			//Fetching the template contents
-			$.get('templates/die-templates.html', function(data) {
-				template = _.template(data, that.model.toJSON());
-				//Option to pass any dynamic values to template
-				that.$el.html(template);
-				//adding the template content to the main template.
-			}, 'html');
-		}
-	});
+   render: function () {
+        var that = this;
+        //Fetching the template contents
+        $.get('templates/die-templates.html', function (data) {
+            template = _.template(data, that.model.toJSON());//Option to pass any dynamic values to template
+            that.$el.html(template);//adding the template content to the main template.
+        }, 'html');
+    }
+});
 
-	var diceView = Backbone.View.extend({
+app.views.Dice = Backbone.View.extend({
 
-		initialize : function(dice) {
-			this.collection = new app.collections.Dice(dice);
-			this.render();
-		},
-
-		render : function() {
-			var that = this;
-			$('#content').empty();
-			_.each(this.collection.models, function(die) {
-				that.renderDie(die);
-			}, this);
-		},
-
-		renderDie : function(die) {
-			var newdie = new app.views.Die({
-				model : die
-			});
-			newdie.render();
-		}
-	});
 	
-	return diceView;
+	initialize: function(dice) {
+		this.collection = new app.collections.Dice(dice);
+		this.render();
+	},
 
+	render : function() {
+		var that = this;
+		$('#content').empty();
+		_.each(this.collection.models, function(die) {
+			that.renderDie(die);
+		}, this);
+	},
+
+	renderDie : function(die) {
+		var newdie = new app.views.Die({
+			model : die
+		});
+		newdie.render();
+	}
 });
